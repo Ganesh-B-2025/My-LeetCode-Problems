44b9c63d56d107321400b53c8e2e93a0aefeb741
Ganesh-B-2025 <ganesh.2025.in@gmail.com>
2025-08-12 22:23:45 +0530

    question 1672 added

--- src/com/leetcode/problems/Question1672.java
+++ src/com/leetcode/problems/Question1672.java
@@ -1,5 +1,29 @@
 package com.leetcode.problems;
 
 public class Question1672 {
-
+	
+	public static int maximumWealth(int[][] arr) {
+		int richestWealth = 0;
+		for(int person=0; person<arr.length; person++) {
+			int currentPersonWealth = 0;
+			for(int account=0; account<arr[person].length; account++) {
+				currentPersonWealth += arr[person][account];
+			}
+			if(currentPersonWealth > richestWealth) {
+				richestWealth = currentPersonWealth;
+			}
+		}
+		return richestWealth;
+	}
+	
+	
+	public static void main(String[] args) {
+		int[][] accounts = { 
+								{ 1, 2, 3 }, 
+								{ 3, 2, 1 },
+								{8, 9, 1}
+							};
+		int ans = maximumWealth(accounts);
+		System.out.println(ans);
+	}
 }
