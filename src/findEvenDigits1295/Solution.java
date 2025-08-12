4b778621740a0759b739ba485f2de6c6b79c5461
Ganesh-B-2025 <ganesh.2025.in@gmail.com>
2025-08-12 19:53:00 +0530

    solved QuestionNumber 1295

---
+++ src/findEvenDigits1295/Solution.java
@@ -0,0 +1,37 @@
+package findEvenDigits1295;
+
+class Solution {
+
+    static int checkHowManyDigits(int n){
+        int count = 0;
+        if(n<0){
+            n = n*-1;
+        }
+        if(n == 0)
+            return 0;
+        while(n>0){
+            count++;
+            n /= 10;
+        }
+        return count;
+    }
+
+    static boolean checkEvenDigits(int n){
+       int digits = checkHowManyDigits(n);
+       return (digits % 2 ==0);
+    }
+    public static int findNumbers(int[] nums) {
+        int count=0;
+        for(int num : nums){
+            if(checkEvenDigits(num))
+                count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {12, 345, 2, 6, 7896, -23};
+        int ans = findNumbers(nums);
+        System.out.println(ans);
+    }
+}
